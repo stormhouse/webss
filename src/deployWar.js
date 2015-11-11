e6b79ba67fc64555d9953867d95a7193c65462ce
stormhouse <stormhouse@yeah.net>
2015-11-11 10:59:15 +0800

    add mvn clean

--- src/deployWar.js
+++ src/deployWar.js
@@ -47,6 +47,51 @@ function shutdownTomcat(callback){
         callback && callback();
     });
 }
+
+function execMaven(mvnExec, arg, callback){
+    console.log('info: mvn '+ arg +' ...')
+
+    var args = []
+
+    if (util.isWin) {
+        args.unshift(mvnExec);
+        args.unshift('/c'),
+            args.unshift('/s');
+        args.push(arg);
+        args.push('-Dmaven.test.skip=true');
+        mvnExec = process.env.COMSPEC || 'cmd.exe';
+    }
+
+    var mvnPackage    = cp.spawn(mvnExec, args, { cwd:  config.currentPath });
+
+    mvnPackage.stdout.on('data', function (data) {
+        console.log(data.toString());
+    });
+
+    mvnPackage.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+
+    mvnPackage.on('exit', function (code) {
+        if(arg === 'clean'){
+            callback && callback()
+            return
+        }
+
+        if(!fs.existsSync(config.sourceWar)){
+            console.error('error: ' + config.sourceWar + ' is not found!!!  please check "contextName && webPath" in webss.json');
+            return
+        }
+        cpy([config.sourceWar], path.join(config.tomcatHome, '/webapps'), function (err) {
+            console.log('info: deploy : \n' + config.sourceWar + ' succeed ')
+            console.log('    ' + path.join(config.tomcatHome, '/webapps'))
+            fs.renameSync(path.join(config.tomcatHome, '/webapps/', config.contextName+'.war') , path.join(config.tomcatHome, '/webapps/', 'ROOT.war') )
+            callback && callback();
+        });
+
+    });
+}
+
 exports.deploy = function(callback) {
     if (!fs.existsSync(config.homePath + '/' + config.tomcatName) || !fs.existsSync(config.homePath + '/' + config.mvnName)) {
         console.log('error: please exec webss setup first !!!');
@@ -79,43 +124,12 @@ exports.deploy = function(callback) {
 
                     updateTomcatPort();
                     shutdownTomcat(function () {
-                        console.log('info: mvn package ...')
-
-                        var args = []
-
-                        if (util.isWin) {
-                            args.unshift(packageExec);
-                            args.unshift('/c'),
-                            args.unshift('/s');
-                            args.push('package');
-                            args.push('-Dmaven.test.skip=true');
-                            packageExec = process.env.COMSPEC || 'cmd.exe';
-                        }
-
-                        var mvnPackage    = cp.spawn(packageExec, args, { cwd:  config.currentPath });
-
-                        mvnPackage.stdout.on('data', function (data) {
-                            console.log(data.toString());
-                        });
-
-                        mvnPackage.stderr.on('data', function (data) {
-                            console.log('stderr: ' + data);
-                        });
-
-                        mvnPackage.on('exit', function (code) {
-
-                                if(!fs.existsSync(config.sourceWar)){
-                                    console.error('error: ' + config.sourceWar + ' is not found!!!  please check "contextName && webPath" in webss.json');
-                                    return
-                                }
-                                cpy([config.sourceWar], path.join(config.tomcatHome, '/webapps'), function (err) {
-                                    console.log('info: deploy : \n' + config.sourceWar + ' succeed ')
-                                    console.log('    ' + path.join(config.tomcatHome, '/webapps'))
-                                    fs.renameSync(path.join(config.tomcatHome, '/webapps/', config.contextName+'.war') , path.join(config.tomcatHome, '/webapps/', 'ROOT.war') )
-                                    callback && callback();
-                                });
-
-                        });
+                        console.log(1)
+                        execMaven(packageExec, 'clean', function(){
+                            execMaven(packageExec, 'package', function(){
+                                callback && callback()
+                            })
+                        })
                     })
                 }
             })
